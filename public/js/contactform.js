9d6b9590e6ea1dbf5ba4edd7f79cdb031b2229d5
ace boogie <acmotanya@gmail.com>
2012-12-29 13:12:38 -0500

    undid initial test close button

--- public/js/contactform.js
+++ public/js/contactform.js
@@ -9,13 +9,8 @@ $("body").append("<div id='ezcontact' style='position:relative; padding:5px; wid
  "<textarea style='width:280px;height:100px; font-family: Helvetica, sans-serif:font-size: 14px;color:black;' name='message' placeholder='Your Message Goes Here'></textarea><br/><input type='submit' value='Send'/></form><p style='display:none;'>Email sent, thanks!</p>"+
  "<a id='ezcontactclose' style='position:absolute;top:5px;right:5px;font-family: Helvetica, sans-serif:font-size: 14px;color:red;' href='#'>Close</a></div>")
 
-$("#ezcontactlink").click(function(){
+$("#ezcontactlink,#ezcontactclose").click(function(){
 	console.log("test")
-	$("#ezcontact").toggle();return false;
-})
-
-$("#ezcontactclose").click(function(){
-	console.log("test1")
 	$("#ezcontact").toggle();return false;
 })
 
