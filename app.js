6e505a3611d37cf503478821b37e95626102aa64
ace boogie <acmotanya@gmail.com>
2013-01-02 07:57:24 -0500

    proper heoku name

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.get('/', function(req, res) {
 							, body: {
 								"script_tag": {
 									"event": "onload",
-									"src": "http://aqueous-thicket-4736/js/contactform.js"
+									"src": "http://aqueous-thicket-4736.herokuapp.com/js/contactform.js"
 								}
 							}
 							, json:true
