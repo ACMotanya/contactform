88b7883538bb1930249e39ac6eabbfda26080c1a
ace boogie <acmotanya@gmail.com>
2013-01-15 08:48:35 -0500

    !important great gatsby

--- public/js/contactform.js
+++ public/js/contactform.js
@@ -3,7 +3,7 @@ jQuery("body").append("<div id='ezcontactlink' style='"+
   //"-webkit-transform-origin: 50% 50%;-moz-transform-origin: 50% 50%;-ms-transform-origin: 50% 50%;-o-transform-origin: 50% 50%;transform-origin: 50% 50%;"+
   "filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=3);"+
 "border:2px solid black;-moz-border-radius: 3px;border-radius: 3px;background: white;padding: 2px;position:fixed;top:95px;left:-28px;width:75px;height:20px"+
-"'><a style='font-family: Helvetica, sans-serif !important:font-size:14px;color:black;' href='#'>Contact Us</a></div>")		
+"'><a style='font-family: Helvetica, sans-serif ! important:font-size:14px ! important;color:black;' href='#'>Contact Us</a></div>")		
 jQuery("body").append("<div id='ezcontact' style='position:relative; padding:5px; width: 300px;border:5px solid black;-moz-border-radius: 15px;border-radius: 15px;display:none;position:fixed;"+
  "top: 100px;left: 200px;z-index:1000;background-color:white;'><form><input type='text' name='name' placeholder='Your Name Here' style='font-family: Helvetica, sans-serif:font-size: 14px;color:black;'><br/><input type='text' style='font-family: Helvetica, sans-serif:font-size: 14px;color:black;' name='email_from' placeholder='Your Email Here' ><br/>"+
  "<textarea style='width:280px;height:100px; font-family: Helvetica, sans-serif:font-size: 14px;color:black;' name='message' placeholder='Your Message Goes Here'></textarea><br/><input type='submit' value='Send'/></form><p style='display:none;'>Email sent, thanks!</p>"+
