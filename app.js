a706be14cbacab9f6131beff1f5f083db1bccd3f
ace boogie <acmotanya@gmail.com>
2013-01-16 21:17:34 -0500

    changing back charge

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.get('/', function(req, res) {
 							console.log(shop)			
 							//I set this disabled flag here since they haven't paid yet. Once we have confirmed they paid we remove it and install the contact form tab
 							//notice it is only set if the PRICE variable is not '0.00' (free)
-							if(PRICE != '7.50') shop.disabled = true	
+							if(PRICE != '0.00') shop.disabled = true	
 							console.log('attempting to save: ' + req.query["shop"]+"_config")					
 							db.collection(req.query["shop"]+"_config", function(err, shop_config) {
 								shop_config.insert(shop, function(err, data) {
@@ -109,7 +109,7 @@ app.get('/', function(req, res) {
 							console.log('SHOP already installed !!!')
 							res.render("index",{ locals:{installed:true}})
 						} else {
-							if(PRICE != '7.50') {
+							if(PRICE != '0.00') {
 								console.log('not in free mode, creating charge')
 								//app is in charge mode so create an app charge
 								createOneTimeCharge(shop, function(err, data){
