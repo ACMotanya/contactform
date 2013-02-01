58b323c7f22c94c2843880ee8ca2bc0eb28764b3
ace boogie <acmotanya@gmail.com>
2013-01-31 21:56:24 -0500

    style change

--- public/js/contactform.js
+++ public/js/contactform.js
@@ -5,7 +5,7 @@ jQuery("body").append("<div id='ezcontactlink' style='"+
 "border:2px solid black;-moz-border-radius: 3px;border-radius: 3px;background: white;padding: 2px;position:fixed;top:95px;left:-28px;width:75px;height:20px"+
 "'><a style='font-family: Helvetica, sans-serif;font-size:14px; color:black;' href='#'>Contact Us</a></div>")		
 jQuery("body").append("<div id='ezcontact' style='position:relative; padding:5px; width: 300px;border:5px solid black;-moz-border-radius: 15px;border-radius: 15px;display:none;position:fixed;"+
- "top: 100px;left: 200px;z-index:1000;background-color:white;'><form><input type='text' name='name' placeholder='Your Name Here' style='font-family: Helvetica, sans-serif:font-size: 14px;color:black;'><br/><input type='text' style='font-family: Helvetica, sans-serif:font-size: 14px;color:black;' name='email_from' placeholder='Your Email Here' ><br/>"+
+ "top: 100px;left: 200px;z-index:99999;background-color:white;'><form><input type='text' name='name' placeholder='Your Name Here' style='font-family: Helvetica, sans-serif:font-size: 14px;color:black;'><br/><input type='text' style='font-family: Helvetica, sans-serif:font-size: 14px;color:black;' name='email_from' placeholder='Your Email Here' ><br/>"+
  "<textarea style='width:280px;height:100px; font-family: Helvetica, sans-serif:font-size: 14px;color:black;' name='message' placeholder='Your Message Goes Here'></textarea><br/><input type='submit' value='Send'/></form><p style='display:none;'>Email sent, thanks!</p>"+
  "<a id='ezcontactclose' style='position:absolute;top:5px;right:5px;font-family: Helvetica, sans-serif:font-size: 14px;color:red;' href='#'>Close</a></div>")
 
