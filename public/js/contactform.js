8bac9ae84a0ea530ccfde29e866fb12501d07e35
Nicholas Manousos <nmanousos@gmail.com>
2013-02-08 15:07:54 -0800

    changed .live() to .on() for compatibility with jquery 1.9. Also changed z-index of the contact form tab to make sure it is on top of large headers.

--- public/js/contactform.js
+++ public/js/contactform.js
@@ -2,19 +2,17 @@ jQuery("body").append("<div id='ezcontactlink' style='"+
  "-webkit-transform: rotate(-90deg); -moz-transform: rotate(-90deg); -ms-transform: rotate(-90deg); -o-transform: rotate(-90deg);"+
  "-webkit-transform-origin: top left; -moz-transform-origin: top left; -ms-transform-origin: top left; -o-transform-origin: top left;"+ 
   "filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=3);"+
-"border:2px solid black;-moz-border-radius: 3px;border-radius: 3px;background: white;padding: 2px;position:fixed;top:150px;left:0px;width:75px;height:20px"+
+"border:2px solid black;-moz-border-radius: 3px;border-radius: 3px;background: white;padding: 2px;position:fixed;z-index:99999;top:150px;left:0px;width:75px;height:20px"+
 "'><a style='font-family: Helvetica, sans-serif;font-size:14px; color:black;' href='#'>Contact Us</a></div>")		
 jQuery("body").append("<div id='ezcontact' style='position:relative; padding:5px; width: 300px;border:5px solid black;-moz-border-radius: 15px;border-radius: 15px;display:none;position:fixed;"+
  "top: 100px;left: 200px;z-index:99999;background-color:white;'><form><input type='text' name='name' placeholder='Your Name Here' style='font-family: Helvetica, sans-serif:font-size: 14px;color:black;'><br/><input type='text' style='font-family: Helvetica, sans-serif:font-size: 14px;color:black;' name='email_from' placeholder='Your Email Here' ><br/>"+
  "<textarea style='width:280px;height:100px; font-family: Helvetica, sans-serif:font-size: 14px;color:black;' name='message' placeholder='Your Message Goes Here'></textarea><br/><input type='submit' value='Send'/></form><p style='display:none;'>Email sent, thanks!</p>"+
  "<a id='ezcontactclose' style='position:absolute;top:5px;right:5px;font-family: Helvetica, sans-serif:font-size: 14px;color:red;' href='#'>Close</a></div>")
 
-jQuery("#ezcontactlink").live("click", function(){
-	//console.log("test")
+jQuery(document).on("click", "#ezcontactlink", function(){
 	jQuery("#ezcontact").show();return false;
 })
-jQuery("#ezcontactclose").live("click", function(){
-	//console.log("test1")
+jQuery(document).on("click", "#ezcontactclose", function(){
 	jQuery("#ezcontact").hide();return false;
 })
 
