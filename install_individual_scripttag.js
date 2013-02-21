5346543fc8102e2d0362eaa65c7285cb16b20221
Nicholas Manousos <nmanousos@gmail.com>
2013-02-21 12:49:00 -0800

    script for installing script tag individually

---
+++ install_individual_scripttag.js
@@ -0,0 +1,15 @@
+var request = require('request')
+
+request.post({
+	url: 'https://essential-oil.myshopify.com/admin/script_tags.json'
+	, headers: { "X-Shopify-Access-Token" : "030d4c727c32c17d3f4278593e108c0f" }
+	, body: {
+		"script_tag": {
+			"event": "onload",
+			"src": "http://aqueous-thicket-4736.herokuapp.com/js/contactform.js"
+		}
+	}
+	, json:true
+	}, function(req, res){
+		console.log(res.body)
+})	
